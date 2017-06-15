59b10b9c4b16ce6801ac8204152bb34633e869ff
Aidana <at33859n@pace.edu>
2017-06-15 18:13:11 +0600

    first version

---
+++ MyToDo/Protocols.swift
@@ -0,0 +1,13 @@
+//
+//  Protocols.swift
+//  MyToDo
+//
+//  Created by Айдана Токбаева on 6/15/17.
+//  Copyright © 2017 Aidana Tokbayeva. All rights reserved.
+//
+
+import UIKit
+
+protocol Communicatable {
+    func get(message: String?)
+}
